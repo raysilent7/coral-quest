7285937aa3430d692289dd4e895a2fc3fe4853ba
raysilent7 <53231079+raysilent7@users.noreply.github.com>
2026-03-04 22:13:46 -0300

    more areas
     - Zona 4 Area 2
     - Zona 4 Area 3
     - Zona 4 Area 4
     - Zona 4 Area 5
     - Zona 4 Area 6
     - Scripts para as areas criadas
     - Ajustes nos scripts de algumas areas anteriores que estavam trocados

---
+++ scripts/areas/Z4area5.gd
@@ -0,0 +1,12 @@
+extends Node2D
+
+@onready var player: CharacterBody2D = $diver/diver
+
+func _ready() -> void:
+	var spawnPoint = get_node_or_null(GameState.lastSpawnId)
+	var fromPortal = GameState.fromPortal
+
+	GameState.isDark = true
+
+	if fromPortal and spawnPoint:
+		player.global_position = spawnPoint.global_position
