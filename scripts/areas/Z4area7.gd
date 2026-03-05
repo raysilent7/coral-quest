1c4317123bbf50d58cad6e1f48afe6f832430b9b
raysilent7 <53231079+raysilent7@users.noreply.github.com>
2026-03-05 00:46:24 -0300

    mais areas
     - Zona 4 area 7
     - ajustes nas marcaçoes de entrada e saida de algumas areas anteriores

---
+++ scripts/areas/Z4area7.gd
@@ -0,0 +1,16 @@
+extends Node2D
+
+@onready var player: CharacterBody2D = $diver/diver
+
+func _ready() -> void:
+	var spawnPoint = get_node_or_null(GameState.lastSpawnId)
+	var shrinePoint = get_node_or_null(GameState.lastShrineId)
+	var fromPortal = GameState.fromPortal
+
+	get_tree().set_pause(false)
+	GameState.isDark = true
+
+	if fromPortal and spawnPoint:
+		player.global_position = spawnPoint.global_position
+	elif not fromPortal and shrinePoint:
+		player.global_position = shrinePoint.global_position
